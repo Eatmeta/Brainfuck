69e038fd2c0d8f83fe8da60f86d9b8528f64a4dd
Eatmeta <metagroul@gmail.com>
2022-11-08 13:16:33 +0300

    Implement a virtual machine

--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class VirtualMachine : IVirtualMachine
-	{
-		public string Instructions { get; }
-		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; }
-		public int MemoryPointer { get; set; }
+    public class VirtualMachine : IVirtualMachine
+    {
+        public string Instructions { get; }
+        public int InstructionPointer { get; set; }
+        public byte[] Memory { get; }
+        public int MemoryPointer { get; set; }
 
-		public VirtualMachine(string program, int memorySize)
-		{
-		}
+        private readonly Dictionary<char, Action<IVirtualMachine>> _dictionary =
+            new Dictionary<char, Action<IVirtualMachine>>();
 
-		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-		{
-			throw new NotImplementedException();
-		}
+        public VirtualMachine(string program, int memorySize)
+        {
+            Instructions = program;
+            Memory = new byte[memorySize];
+        }
 
-		public void Run()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+        {
+            _dictionary.Add(symbol, execute);
+        }
+
+        public void Run()
+        {
+            while (InstructionPointer < Instructions.Length)
+            {
+                if (_dictionary.ContainsKey(Instructions[InstructionPointer]))
+                    _dictionary[Instructions[InstructionPointer]](this);
+                InstructionPointer++;
+            }
+        }
+    }
 }
