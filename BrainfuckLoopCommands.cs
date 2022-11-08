34bd0d47abc8ec48de6bd6605c1e62cbfb906722
Eatmeta <metagroul@gmail.com>
2022-11-08 13:21:17 +0300

    Implement a method RegisterTo that logs loop commands to the virtual machine

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -2,12 +2,39 @@ using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
-	}
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var openBraceIndex = new Stack<int>();
+            var openBracesPositions = new Dictionary<int, int>();
+            var closeBracesPositions = new Dictionary<int, int>();
+			
+            for (var i = 0; i < vm.Instructions.Length; i++)
+            {
+                switch (vm.Instructions[i])
+                {
+                    case '[':
+                        openBraceIndex.Push(i);
+                        break;
+                    case ']':
+                        openBracesPositions.Add(openBraceIndex.Peek(), i);
+                        closeBracesPositions.Add(i, openBraceIndex.Pop());
+                        break;
+                }
+            }
+			
+            vm.RegisterCommand('[', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = openBracesPositions[b.InstructionPointer];
+            });
+			
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                    b.InstructionPointer = closeBracesPositions[b.InstructionPointer];
+            });
+        }
+    }
 }
